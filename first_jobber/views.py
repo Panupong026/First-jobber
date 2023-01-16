60039b9e562a7e59ff10312604fcad1b598b8513
panupong <panuponglovecal@gmail.com>
2023-01-16 12:48:04 +0700

    serializers

--- first_jobber/views.py
+++ first_jobber/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import InsuranceSerializer, UserSerializer, CoverageSerializer
+from .models import Insurance, User, Coverage
 
 # Create your views here.
+
+class InsuranceList(generics.ListCreateAPIView):
+    queryset = Insurance.objects.all()
+    serializer_class = InsuranceSerializer
+
+class InsuranceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Insurance.objects.all()
+    serializer_class = InsuranceSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class CoverageList(generics.ListCreateAPIView):
+    queryset = Coverage.objects.all()
+    serializer_class = CoverageSerializer
+
+class CoverageDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Coverage.objects.all()
+    serializer_class = CoverageSerializer
